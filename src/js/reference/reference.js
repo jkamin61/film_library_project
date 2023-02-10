0692a702f371e85e15cea3fb795f4d2256009ed8
AdrianSiwek <105585774+AdrianSiwek@users.noreply.github.com>
2023-02-10 23:46:11 +0100

    to check pagination, loader

---
+++ src/js/reference/reference.js
@@ -0,0 +1,6 @@
+//Use here the stuff you want to load the loader on//
+
+export const reference = {
+    loader: document.querySelector('.loader'),
+    overlay: document.querySelector('.overlay'),
+}
