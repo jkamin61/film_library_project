378470e8f114100d846123c00fbe2198440be9c8
soutdev <107152863+jkamin61@users.noreply.github.com>
2023-02-15 15:22:05 +0100

    login 1.1
    continued work on login form and few fixes

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import { createHomeCard, createLibraryCard } from './js/createCard';
 import { compileGenreDictionary } from './js/compileGenreDictionary';
 import { FetchMoveApi } from './js/FetchMove';
 import './js/openFooterMdl';
+import { modalFunctions } from './js/userLogin';
 const wrapper = document.querySelector('.wrapper');
 const PER_PAGE = 20;
 
@@ -16,3 +17,6 @@ get_movies.getTrendDay(1).then(data => {
     });
   });
 });
+
+
+modalFunctions();
--- src/js/userLogin.js
+++ src/js/userLogin.js
@@ -1,3 +1,4 @@
+export {modalFunctions};
 import { initializeApp } from "firebase/app";
 const firebaseConfig = {
   apiKey: "AIzaSyA1p5zFRYitESqi8tfXfMidfyvRPHCvW00",
@@ -8,3 +9,18 @@ const firebaseConfig = {
   appId: "1:70753383955:web:59d9b4c26be7561d35b1e6"
 };
 const app = initializeApp(firebaseConfig);
+const loginOpeningFormBtn = document.querySelector(".login__button")
+const closeRegisterModal = document.querySelector(".login__close");
+const modalWindow = document.querySelector(".login__modal");
+const modalFunctions = () => {
+
+
+  loginOpeningFormBtn.addEventListener("click", (event) => {
+    modalWindow.style.display = "flex";
+  })
+
+  closeRegisterModal.addEventListener("click", (event) => {
+    event.preventDefault();
+    modalWindow.style.display = "none";
+  })
+}
