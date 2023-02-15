d6e22cc6f4a504a0b5e7e082f56687205ff131c4
soutdev <107152863+jkamin61@users.noreply.github.com>
2023-02-15 10:39:42 +0100

    login-in-progress

---
+++ src/js/userLogin.js
@@ -0,0 +1,10 @@
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyA1p5zFRYitESqi8tfXfMidfyvRPHCvW00",
+  authDomain: "film-library-499b4.firebaseapp.com",
+  projectId: "film-library-499b4",
+  storageBucket: "film-library-499b4.appspot.com",
+  messagingSenderId: "70753383955",
+  appId: "1:70753383955:web:59d9b4c26be7561d35b1e6"
+};
+const app = initializeApp(firebaseConfig);
