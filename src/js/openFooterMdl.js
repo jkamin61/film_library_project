7c0c8f18ad4050b56ead80191d650dd64d0345e5
Dawid Bazyluk <dawid.bazyluk@gmail.com>
2023-02-16 01:49:50 +0100

    footer-finish
    fix footer size, add closing modal by hit ESC key and click on clapper

--- src/js/openFooterMdl.js
+++ src/js/openFooterMdl.js
@@ -1,14 +1,29 @@
 const footerLink = document.querySelector('[data-team]');
 const closeBtnFooter = document.querySelector('.footer__close');
 const footerModal = document.querySelector('.modal-container');
+const clapper = document.querySelector('.footer__clapper');
 
 function openModal() {
   footerModal.classList.toggle('is-hidden');
+  document.addEventListener('keydown', escClick);
 }
 
 function closeModal() {
   footerModal.classList.toggle('is-hidden');
+  document.removeEventListener('keydown', escClick);
 }
 
+function escClick(e) {
+  if (e.keyCode === 27) {
+    closeModal();
+  }
+}
+
+function closeClick(e) {
+  if (e.target === clapper) {
+    closeModal();
+  }
+}
 footerLink.addEventListener('click', openModal);
 closeBtnFooter.addEventListener('click', closeModal);
+clapper.addEventListener('click', closeClick);
