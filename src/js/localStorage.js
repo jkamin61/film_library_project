09c3934862a0c095c856090d3290e4fc3181abf6
asiaostrowska55 <110023821+asiaostrowska55@users.noreply.github.com>
2023-02-19 10:36:33 +0100

    update

--- src/js/localStorage.js
+++ src/js/localStorage.js
@@ -6,10 +6,6 @@ import { genreIdToList, createHomeCard, createLibraryCard } from './createCard';
 import { openMovieInfoModal } from './openMovieInfoModal';
 import { createMovieInfoCard } from './createMovieInfoCard';
 
-
-const btnWatched = document.querySelector('.movie-info__button--watched');
-const btnQueue = document.querySelector('.movie-info__button--queue');
-
 console.log(btnWatched);
 import './openMovieInfoModal';
 import './createMovieInfoCard';
@@ -18,17 +14,22 @@ const fetch_movie = new FetchMoveApi();
 
 import { createMovieInfoCard } from './createMovieInfoCard';
 
-import { handleOpenClick } from './openMovieInfoModal';
-
-const addToWatched = movie => {
-  localStorage.setItem('watched', JSON.stringify(movie.data));
-};
-const addToQueue = movie => {
-  localStorage.setItem('watched', JSON.stringify(movie.data));
-};
-
-function handleWatchedBtn() {
+//main function
+export function handleButtons() {
   const btnWatched = document.querySelector('.movie-info__button--watched');
+  const btnQueue = document.querySelector('.movie-info__button--queue');
+
+  btnWatched.addEventListener('click', function () {
+    console.log('watched klik');
+    localStorage.setItem('added to watched', 'watched');
+    Notify.success('Movie was added to Watched');
+  });
+  btnQueue.addEventListener('click', function () {
+    console.log('queue klik');
+    localStorage.setItem('added to queue', 'queue');
+    Notify.success('Movie was added to Queue');
+  });
+
 }
 
 //notify adjust
@@ -36,55 +37,9 @@ Notify.init({
   width: '300px',
   position: 'center-top',
   closeButton: false,
-  timeout: 2000,
+  timeout: 1500,
+  showOnlyTheLastOne: true,
   success: {
     background: '#ff6b01',
   },
 });
-
-// ** storage na btn w library
-const storage = () => {
-  btnWatched.addEventListener('click', e => {
-    e.preventDefault();
-    console.log('klik');
-    localStorage.setItem('watched', 'test name');
-    // showLocalStorage();
-    Notify.success('Movie added to Watched');
-  });
-  btnQueue.addEventListener('click', e => {
-    e.preventDefault();
-    console.log('kju');
-    localStorage.setItem('queue', 'value');
-    // showLocalStorage();
-    Notify.success('Movie added to Queue');
-  });
-};
-
-//**  */
-
-
-
-// header.addEventListener('click', storage);
-
-// const storage = () => {
-//   localStorage.setItem('movie details', JSON.stringify(data.results[1]));
-// };
-
-// ** test na local storage - usunąć na koniec
-// window.localStorage.setItem(
-//   'movie',
-//   JSON.stringify({ name: 'imię', movie: 'film' })
-// );
-
-// function showLocalStorage() {
-//   let value = [];
-//   let keys = Object.keys(localStorage);
-//   let i = keys.length;
-//   while (i--) {
-//     value.push(localStorage.getItem(keys[i]));
-//   }
-//   document.querySelector('.film-container').textContent = value;
-// }
-
-// import {}
-// const movie = document.querySelector('.movie-card');
